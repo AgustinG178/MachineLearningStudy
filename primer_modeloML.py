a7caf9bbc6caaa4ab60ce0705ac998ef17b70290
Agustin Grioni <agugrio17@gmail.com>
2025-06-18 19:16:26 -0300

    subo mi prime modelo de ML

---
+++ primer_modeloML.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import seaborn as sb
+from sklearn.linear_model import LinearRegression
+
+# Cargar el conjunto de datos para entrenar el modelo
+datos = pd.read_csv("temperaturas.csv")
+datos.info()
+datos.head()
+
+sb.scatterplot(x="celsius", y="fahrenheit", data=datos,
+               hue="fahrenheit", palette="coolwarm")
+
+#Caracteristicas (X), etiqueta (y)
+X = datos["celsius"]
+y = datos["fahrenheit"]
+
+X_procesada = X.values.reshape(-1,1)
+y_procesada = y.values.reshape(-1,1)
+
+
+modelo = LinearRegression()
+
+modelo.fit(X_procesada, y_procesada)
+
+celsius = 0
+
+while celsius != -999:
+    
+    celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+    prediccion = modelo.predict([[celsius]])
+    print(f"{celsius} grados celsius son {prediccion} grados fahrenheit")
+
+    modelo.score(X_procesada, y_procesada)
+    
+    if celsius == -999:
+        print("terminando de predecir...")
+        break
+        
+        
+        
